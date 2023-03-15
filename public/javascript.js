54397bf776c971bbf9df70b6eef054ebdea472de
PKenneth01 <kennethkabutu@rocketmail.com>
2023-03-15 20:34:32 +0300

    js code for alert

--- public/javascript.js
+++ public/javascript.js
@@ -1,3 +1,13 @@
-function today(){
-    get hello
-}
+const form = document.getElementById("phone number");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); // prevent form from submitting
+
+    const numberInput = document.getElementById("number");
+    const numberValue = numberInput.value;
+
+    if (isNaN(numberValue)) {
+      alert("Please enter a valid phone number.");
+    } else {
+      form.submit();
+    }
+  });
