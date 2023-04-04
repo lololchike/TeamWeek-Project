06dd788e8dd3387f65b2ca948c8da5fcf2ff614b
Bill <billotungah@gmail.com>
2023-04-04 10:35:25 +0300

    deploy build

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,17 @@ const app = express()
 const mongoose = require('mongoose')
 require('dotenv').config();
 const router = require("./api/routes/route")
-//app.get("/",(req,res)=>{res.send("You are Here!")})
+const path = require("path")
+const _dirname = path.dirname("")
+const buildPath = path.join(_dirname, "../frontend/build")
+
+app.use(express.static(buildPath))
+
+app.get("/", (req, res)=> {
+res.sendFile(
+    path.join(__dirname, "../frontend/build/index.html"))
+})
+
 app.use(express.static("../frontend1/public"))
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true})); 
